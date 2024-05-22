e740149f5a956faeff5ab2b898dfe27fe091161e
unknown <mohammadbaghermollanory@gmail.com>
2024-05-22 09:13:09 -0700

    update navbar-brand & nav-link & bag

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,22 @@
 import { BsBag } from "react-icons/bs";
+import './Navbar.css';
 
 const Navbar = () => {
     return (
        
             <nav class="navbar navbar-expand-sm bg-dark d-flex">
                 <div class="container">
-                    <a class="navbar-brand" href="#">Navbar</a>
+                    <a class="navbar-brand" href="#">SHOPPING!!!</a>
 
                     <ul class="navbar-nav me-auto mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link" aria-current="page" href="#">Home</a>
                         </li>
                     </ul>
-                </div>
-                <div>
-                    <BsBag />
+                <div className="bag">
+                    <BsBag style={{color: 'white'}}/>
                     <span className="bg-white">0</span>
+                </div>
                 </div>
             </nav>
 
