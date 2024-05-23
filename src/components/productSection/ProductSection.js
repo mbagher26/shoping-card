b902538db0fdc8622a484e34d6d65f1a841d199b
unknown <mohammadbaghermollanory@gmail.com>
2024-05-23 06:35:58 -0700

    create ProductSection component

---
+++ src/components/productSection/ProductSection.js
@@ -0,0 +1,17 @@
+const Productsection = () => {
+    return (
+        <div className="card text-center" >
+            <h2>Category</h2>
+            <img src="./public/images/1.jpg" />
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+                <p>Number:15</p>
+            </div>
+        </div>
+    );
+}
+
+export default Productsection;
