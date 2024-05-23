f1d020f576d84675e910b5f09bc2426162192df0
unknown <mohammadbaghermollanory@gmail.com>
2024-05-23 07:48:14 -0700

    update card

--- src/components/productSection/ProductSection.js
+++ src/components/productSection/ProductSection.js
@@ -1,14 +1,15 @@
+import './ProductSection.css'
 const Productsection = () => {
     return (
-        <div className="card text-center" >
-            <h2>Category</h2>
-            <img src="./public/images/1.jpg" />
+        <div className="card" >
+            <h1>Category</h1>
+            <img src="/images/1.jpg" />
             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-                <p>Number:15</p>
+                <h5 className="card-title">Samsung A52</h5>
+                <p className="card-text">1200$</p>
+                <a href="#" className="btn btn-danger">Add To Cart</a>
+                <a href="#" className="btn btn-outline-dark mt-2 mb-2 text-capitalize">More information</a>
+                <p >Number:15</p>
             </div>
         </div>
     );
