935a5c11bc27f67bb6cc6dc0dd275b1c7fed35b9
unknown <mohammadbaghermollanory@gmail.com>
2024-05-25 05:19:39 -0700

    import products and set to allProducts & dynamic ProductSection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import ProductSection from './components/productSection/ProductSection';
+import products from './data/products';
 
 function App() {
+  const [allProducts, setAllProducts] = useState(products);
   return (
     <div className="App">
-      <Navbar/>      
+      <Navbar/>     
+      <main className='pb-5'>
+        <div className='container-app'>
+          <h1 className='text-center mt-5'>All Products</h1>
+          {allProducts.map(product => (
+            <ProductSection key={product.id} product={product}/>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
