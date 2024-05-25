94b769845bef9eb53fe24826f28f0e6026c18b6d
unknown <mohammadbaghermollanory@gmail.com>
2024-05-25 05:28:29 -0700

    update Productsection for dynamic and use product props and map

--- src/components/productSection/ProductSection.js
+++ src/components/productSection/ProductSection.js
@@ -1,17 +1,31 @@
 import './ProductSection.css'
-const Productsection = () => {
+const Productsection = ({ product }) => {
+
+    let { title, infos } = product
+
     return (
-        <div className="card" >
-            <h1>Category</h1>
-            <img src="/images/1.jpg" />
-            <div className="card-body">
-                <h5 className="card-title">Samsung A52</h5>
-                <p className="card-text">1200$</p>
-                <a href="#" className="btn btn-danger">Add To Cart</a>
-                <a href="#" className="btn btn-outline-dark mt-2 mb-2 text-capitalize">More information</a>
-                <p >Number:15</p>
+        <>
+
+
+            <div>
+                <h1 className='mt-5'>{title}</h1>
+                <div className='product-info'>
+                    {infos && infos.map(info => (
+                        <div className='card p-3' >
+                            <img src={info.img} />
+                            <div className="card-body">
+                                <h5 className="card-title">{info.title}</h5>
+                                <p className="card-text">{info.price}</p>
+                                <a href="#" className="btn btn-danger">Add To Cart</a>
+                                <a href="#" className="btn btn-outline-dark mt-2 mb-2 text-capitalize">More information</a>
+                                <p >Number:{info.count}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
-        </div>
+
+        </>
     );
 }
 
