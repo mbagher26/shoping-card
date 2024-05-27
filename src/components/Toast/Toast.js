c0fe88c8fe0886092be4abd22c60aa08013f9728
unknown <mohammadbaghermollanory@gmail.com>
2024-05-27 09:32:13 -0700

    change class to className and update style with position and margin

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -1,11 +1,11 @@
 const Toast = () => {
     return (
-        <div class="toast align-items-center text-bg-primary border-0 ms-auto me-1" role="alert" aria-live="assertive" aria-atomic="true">
-            <div class="d-flex">
-                <div class="toast-body">
+        <div className="toast align-items-center text-bg-primary border-0 m-2 position-fixed bottom-0 end-0" role="alert" aria-live="assertive" aria-atomic="true">
+            <div className="d-flex">
+                <div className="toast-body">
                     محصول با موفقیت به سبد خرید اضافه شد.                    
                 </div>
-                <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+                <button type="button" className="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
         </div>
     );
