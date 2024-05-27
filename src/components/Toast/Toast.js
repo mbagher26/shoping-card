d830c1331ff7b795222806bc1857b3417aa729bd
unknown <mohammadbaghermollanory@gmail.com>
2024-05-27 08:59:59 -0700

    create Toast component

---
+++ src/components/Toast/Toast.js
@@ -0,0 +1,14 @@
+const Toast = () => {
+    return (
+        <div class="toast align-items-center text-bg-primary border-0 ms-auto me-1" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="d-flex">
+                <div class="toast-body">
+                    محصول با موفقیت به سبد خرید اضافه شد.                    
+                </div>
+                <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+        </div>
+    );
+}
+
+export default Toast;
