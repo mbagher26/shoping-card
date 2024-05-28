ddb79b30202f09ecceaecbab05b2f89f135c5168
unknown <mohammadbaghermollanory@gmail.com>
2024-05-28 09:21:23 -0700

    shaire data with context

--- src/App.js
+++ src/App.js
@@ -2,26 +2,33 @@ import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import ProductSection from './components/productSection/ProductSection';
-import products from './data/products';
 import Toast from './components/Toast/Toast';
 import Cart from './components/Cart/Cart';
+import DataProvider, { DataContext } from './Context/DataContext';
+import products from './data/products';
 
 function App() {
+
   const [allProducts, setAllProducts] = useState(products);
+  const [userCart, setUserCart] = useState([]);
+  const [showToast, setShowToast] = useState(false);
+  const [showCart, setShowCart] = useState(false);
+
   return (
-    <div className="App">
-      <Navbar/>     
-      <main className='pb-5'>
-        <div className='container-app'>
-          <h1 className='text-center mt-5'>All Products</h1>
-          {allProducts.map(product => (
-            <ProductSection key={product.id} product={product}/>
-          ))}
-        </div>
-      </main>
-      <Toast/>
-      <Cart />
-    </div>
+    <DataContext.Provider value={{allProducts, userCart, showCart, setShowCart, showToast, setShowToast }}>
+      <div className="App">
+        <Navbar />
+        <main className='pb-5'>
+          <div className='container-app'>
+            <h1 className='text-center mt-5'>All Products</h1>
+              <ProductSection />
+          </div>
+        </main>
+        <Toast />
+        <Cart />
+      </div>
+    </DataContext.Provider>
+
   );
 }
 
