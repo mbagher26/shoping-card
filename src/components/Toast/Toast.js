c2e0c4fe1810b8fb2f31ac768177a7e06f7a42cd
unknown <mohammadbaghermollanory@gmail.com>
2024-06-02 05:07:04 -0700

    get data with useContext & update className & declare close button

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -1,11 +1,19 @@
+import { useContext } from "react";
+import { DataContext } from "../../Context/DataContext";
+
 const Toast = () => {
+    const ContextData = useContext(DataContext)
+    
     return (
-        <div className="toast align-items-center text-bg-primary border-0 m-2 position-fixed bottom-0 end-0" role="alert" aria-live="assertive" aria-atomic="true">
+        <div className={` ${ContextData.showToast ? 'show' : ''} toast align-items-center text-bg-primary border-0 m-2 position-fixed bottom-0 end-0`} role="alert" aria-live="assertive" aria-atomic="true">
             <div className="d-flex">
                 <div className="toast-body">
                     محصول با موفقیت به سبد خرید اضافه شد.                    
                 </div>
-                <button type="button" className="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+                <button onClick={() => {
+                    ContextData.setShowToast(false)
+                }} 
+                type="button" className="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
         </div>
     );
