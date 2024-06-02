9ad07641d7ed0e283ab2127e5201e1bdae2a093e
unknown <mohammadbaghermollanory@gmail.com>
2024-06-02 05:02:45 -0700

    get data with useContext and map on data & declare onCllick for Add To Cart button

--- src/components/productSection/ProductSection.js
+++ src/components/productSection/ProductSection.js
@@ -1,22 +1,32 @@
+import { DataContext } from '../../Context/DataContext';
 import './ProductSection.css'
-const Productsection = ({ product }) => {
+import { useContext } from 'react';
 
-    let { title, infos } = product
+const Productsection = () => {
 
+    const ContextData = useContext(DataContext)
+    const allProducts = ContextData.allProducts
+    
     return (
         <>
-
-
+        {allProducts && allProducts.map(product => (
             <div>
-                <h1 className='mt-5'>{title}</h1>
+                <h1 className='mt-5'>{product.title}</h1>
                 <div className='product-info'>
-                    {infos && infos.map(info => (
+                    {product.infos && product.infos.map(info => (
                         <div className='card p-3' >
                             <img src={info.img} />
                             <div className="card-body">
                                 <h5 className="card-title">{info.title}</h5>
                                 <p className="card-text">{info.price}</p>
-                                <a href="#" className="btn btn-danger">Add To Cart</a>
+                                <a href="javascript:void(0)" onClick={() => {ContextData.setShowToast(true)
+                                    setTimeout(() => {
+                                        ContextData.setShowToast(false)
+
+                                    }
+                                        ,3000)
+                                }}
+                                 className="btn btn-danger">Add To Cart</a>
                                 <a href="#" className="btn btn-outline-dark mt-2 mb-2 text-capitalize">More information</a>
                                 <p >Number:{info.count}</p>
                             </div>
@@ -24,6 +34,7 @@ const Productsection = ({ product }) => {
                     ))}
                 </div>
             </div>
+        ))}
 
         </>
     );
