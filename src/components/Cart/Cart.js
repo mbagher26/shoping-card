0ef789ea7a5a3dbeb04b621f30e0bbfebf4d78c8
unknown <mohammadbaghermollanory@gmail.com>
2024-06-08 04:11:37 -0700

    import DataContext & declare onClick for close bag & map on userCart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,29 +1,38 @@
 import { BsBag } from "react-icons/bs";
 import { IoCloseSharp } from "react-icons/io5";
+import { DataContext } from "../../Context/DataContext";
+import { useContext } from "react";
 
 const Cart = () => {
-    return (
-        <aside className="bag-sidebar d-none w-25 p-3 position-fixed top-0 end-0 bg-white shadow h-100">
-            
-                <h3 className="d-flex justify-content-between">
-                    <span><BsBag /> Cart</span>
-                    <span><IoCloseSharp /></span>
-                </h3>
-            
-            <div className='product-info'>
 
-                    <div className='card p-3' >
-                        <img src='/images/1.jpg' />
-                        <div className="card-body d-flex flex-column">
-                            <h5 className="card-title">title</h5>
-                            <p className="card-text">price</p>
-                            <a href="#" className="btn btn-danger">Buy</a>
-                            <a href="#" className="btn btn-outline-dark mt-2 mb-2 text-capitalize">More information</a>
-                            <p >Number:count</p>
-                        </div>
+    const ContextData = useContext(DataContext)
+
+    return (
+        <aside className={`${ContextData.showCart ? '' : 'd-none'} bag-sidebar overflow-auto w-25 p-3 position-fixed top-0 end-0 bg-white shadow h-100`}>
+
+            <h3 className="d-flex justify-content-between">
+                <span><BsBag /> Cart</span>
+                <span onClick={() => {
+                    ContextData.setShowCart(false)
+                }}><IoCloseSharp /></span>
+            </h3>
+
+            {ContextData.userCart.map(product => (
+            <div key={product.id} className='product-info'>
+
+                <div className='card p-3' >
+                    <img src={product.img} />
+                    <div className="card-body d-flex flex-column">
+                        <h5 className="card-title">{product.title}</h5>
+                        <p className="card-text">{product.price}$</p>
+                        <a href="#" className="btn btn-danger">Buy</a>
+                        <a href="#" className="btn btn-outline-dark mt-2 mb-2 text-capitalize">More information</a>
+                        <p >Number: {product.count}</p>
                     </div>
-                
+                </div>
+            
             </div>
+            ))}
         </aside>
     );
 }
